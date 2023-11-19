4923548bb538cf869e033970a3b7424719e50d40
Caleb <caleb.boyum@my.smsu.edu>
2023-11-19 16:03:43 -0600

    Added in my existing code where it seemed useful

--- src/components/header/TopBar.vue
+++ src/components/header/TopBar.vue
@@ -1,3 +1,76 @@
+
+import SearchBar from './SearchBar.vue';
+import UserMenu from './UserMenu.vue';
+
 <template>
-    <h1>TopBar</h1>
+    <div id = "Top">
+        <div id = "Left">
+            <button id = "Logo" class = "top">
+                TicketPort
+            </button>
+        </div>
+        <div id = "Center">
+            <SearchBar/>
+        </div>
+        <div id = "Right">
+            <button class = "top button login">
+                Logout
+            </button>
+            <UserMenu/>
+        </div>
+    </div>
 </template>
+
+<style>
+.login{
+    float: right;
+}
+
+.top{
+    background-color: rgb(61, 93, 133);
+    color: white;
+    padding: 5px;
+    height: 30px;
+    border-radius: 10px;
+}
+
+.button{
+    background-color: rgb(28, 40, 51);
+    color: white;
+    padding: 5px;
+    height: 30px;
+    border-radius: 10px;
+}
+
+#Top{
+    background-color: rgb(61, 93, 133);
+    color: white;
+    padding: 5px;
+    height: 50px;
+    border-radius: 10px;
+}
+
+#Logo{
+    border-width: 0px;
+    font-size: 2em;
+    padding-right: 25px;
+}
+
+#Left{
+    float:left;
+}
+
+#Center{
+    display: flex;
+    margin-top: 10px;
+    width: auto;
+    align-items: center;
+    justify-content: center;
+}
+
+#Right{
+    margin-top: -30px;
+    float: right;
+    width: 200px;
+}
+</style>
