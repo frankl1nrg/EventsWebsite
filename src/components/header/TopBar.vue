5dd1e84433f03a1b4e387572f3c35e0ab3266e4e
Caleb <caleb.boyum@my.smsu.edu>
2023-11-27 16:47:17 -0600

    topbar v7

--- src/components/header/TopBar.vue
+++ src/components/header/TopBar.vue
@@ -13,7 +13,7 @@ import UserMenu from './UserMenu.vue';
             <SearchBar/>
         </div>
         <div id = "Right">
-            <button class = "top button login">
+            <button id = "Button">
                 Logout
             </button>
             <UserMenu/>
@@ -22,24 +22,14 @@ import UserMenu from './UserMenu.vue';
 </template>
 
 <style>
-.login{
-    float: right;
-}
 
-.top{
-    background-color: rgb(61, 93, 133);
-    color: white;
-    padding: 5px;
-    height: 30px;
-    border-radius: 10px;
-}
-
-.button{
+#Button{
     background-color: rgb(28, 40, 51);
     color: white;
     padding: 5px;
     height: 30px;
     border-radius: 10px;
+    float: right;
 }
 
 #Top{
@@ -54,6 +44,11 @@ import UserMenu from './UserMenu.vue';
     border-width: 0px;
     font-size: 2em;
     padding-right: 25px;
+    background-color: rgb(61, 93, 133);
+    color: white;
+    padding: 5px;
+    height: 30px;
+    border-radius: 10px;
 }
 
 #Left{
