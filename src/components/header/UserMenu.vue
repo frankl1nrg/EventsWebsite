4f4930dc4e8da8ea499d0434b489dcb5ace69e2e
Caleb <caleb.boyum@my.smsu.edu>
2023-11-29 09:02:30 -0600

    UserMenu v2

--- src/components/header/UserMenu.vue
+++ src/components/header/UserMenu.vue
@@ -1,35 +1,23 @@
+<script>
+
+</script>
+
 <template>
-  <ejs-dropdownbutton :items='items'>Clipboard</ejs-dropdownbutton>
-  </template>
-  
-  <script>
-  import Vue from 'vue';
-  import { DropDownButtonPlugin } from "@syncfusion/ej2-vue-splitbuttons";
-  import { enableRipple } from '@syncfusion/ej2-base';
-  
-  enableRipple(true);
-  Vue.use(DropDownButtonPlugin);
-  export default {
-      data () {
-          return {
-              items:[
-              {
-                  text: 'Cut'
-              },
-              {
-                  text: 'Copy'
-              },
-              {
-                  text: 'Paste'
-              }]
-          };
-      }
-  }
-  </script>
-  
-  <style>
+  <div>
+    <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+      <b-dropdown-item>First Action</b-dropdown-item>
+      <b-dropdown-item>Second Action</b-dropdown-item>
+      <b-dropdown-item>Third Action</b-dropdown-item>
+      <b-dropdown-divider></b-dropdown-divider>
+      <b-dropdown-item active>Active action</b-dropdown-item>
+      <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<style>
   @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
   @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
   @import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
   @import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
-  </style>
+</style>
