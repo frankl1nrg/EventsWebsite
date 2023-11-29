cb0171afcdc4013f4717f3aee97a45a6b312a064
Caleb <caleb.boyum@my.smsu.edu>
2023-11-29 09:04:45 -0600

    UserMenu quickFix

--- src/components/header/UserMenu.vue
+++ src/components/header/UserMenu.vue
@@ -16,8 +16,5 @@
 </template>
 
 <style>
-  @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
-  @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
-  @import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
-  @import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+
 </style>
