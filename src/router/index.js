628bbfdc9abf5cde2f2d1e4e287f9aac269cb7e8
Caleb <caleb.boyum@my.smsu.edu>
2023-12-01 11:23:49 -0600

    Index fix #2

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import EventInfo from '../views/EventInfo.vue'
 import MyEvents from '../views/MyEvents.vue'
 import MyReservations from '../views/MyReservations.vue'
 import SearchResults from '../views/SearchResults.vue'
+import Profile from '../views/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
