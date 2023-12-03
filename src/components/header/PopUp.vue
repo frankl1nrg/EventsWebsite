556091f7f0ec2b0304db9c766e18a816cfc2934d
Franklin Roa Guerrero <franklinroaguerrero@gmail.com>
2023-12-03 12:01:52 -0600

    fix login and user menu

---
+++ src/components/header/PopUp.vue
@@ -0,0 +1,27 @@
+<template setup>
+  <div class="pop-up">
+    <div class="popup-inner">
+      <slot/>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+.pop-up {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0,0,0,0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
