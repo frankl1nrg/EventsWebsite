641b7c8de7dec5a47f7b6b64555b26d245d5d1b0
Franklin Roa Guerrero <franklinroaguerrero@gmail.com>
2023-12-03 21:17:09 -0600

    fix path

--- src/views/MyReservations.vue
+++ src/views/MyReservations.vue
@@ -1,6 +1,6 @@
 <template>
     <div class ="not-logged-in">
-        <img src="src/assets/images/notLoggedInMyReservations.png" alt="not logged in" class="center">
+        <img src="public/images/notLoggedInMyReservations.png" alt="not logged in" class="center">
     </div>
 </template>
 
