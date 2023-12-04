a978e7af0a484740163e5a3f7daf129890b2d4c5
Caleb <caleb.boyum@my.smsu.edu>
2023-12-04 10:46:53 -0600

    Removed unneccessary style elements

--- src/views/MyEvents.vue
+++ src/views/MyEvents.vue
@@ -70,30 +70,6 @@ setup() {
 </script>
 
 <style>
-  .menu{
-      align-items: center;
-      justify-content: center;
-  }
-
-  .input{
-      background-color: rgb(255, 255, 255);
-      color: black;
-      padding: 5px;
-      height: 30px;
-      border-radius: 10px;
-  }
-
-  .loginButton{
-      display: block;
-      background-color: rgb(28, 40, 51);
-      margin: auto;
-      margin-top: 30px;
-      color: white;
-      height: 40px;
-      width: 100px;
-      border-radius: 10px;
-  }
-
   .center{
       display: block;
       margin: auto;
